8883d897f36d5f7a75b9eb5553472824771d3cc3
Renan Beux <nanerxueb@hotmail.com>
2018-12-03 16:02:47 -0200

    03122018 1602

--- TesteOneSignal/TesteOneSignal/App.xaml.cs
+++ TesteOneSignal/TesteOneSignal/App.xaml.cs
@@ -12,13 +12,13 @@ namespace TesteOneSignal
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
-
             if (Device.RuntimePlatform != Device.UWP)
             {
                 OneSignal.Current.StartInit("efd8208c-6f2e-4dff-ba45-d8865fbd2610").EndInit();
                 OneSignal.Current.IdsAvailable(IdsAvaiable);
             }
+
+            MainPage = new MainPage();
         }
               
         protected override void OnStart()
@@ -32,10 +32,10 @@ namespace TesteOneSignal
 
         private async void IdsAvaiable(string userID, string pushToken)
         {
-            if (App.Current.Properties["userID"].ToString() != userID)
+            if ((!App.Current.Properties.ContainsKey("userID")) || (App.Current.Properties["userID"].ToString() != userID))
             {
-                App.Current.Properties["userID"] = userID;
-                App.Current.Properties["pushToken"] = pushToken;
+                App.Current.Properties.Add("userID", userID);
+                App.Current.Properties.Add("pushToken", pushToken);
                 await App.Current.SavePropertiesAsync();
             }
         }
--- TesteOneSignal/TesteOneSignal/MainPage.xaml.cs
+++ TesteOneSignal/TesteOneSignal/MainPage.xaml.cs
@@ -6,47 +6,52 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Com.OneSignal;
 using Com.OneSignal.Abstractions;
+using System.Net.Http;
+using System.Net;
 
 namespace TesteOneSignal
 {
     public partial class MainPage : ContentPage
-    {
+    {  
         public MainPage()
         {
             InitializeComponent();
+            timePicker.Time = DateTime.Now.TimeOfDay;
+            
             if (App.Current.Properties.ContainsKey("userID"))
                 lblIdUser.Text = App.Current.Properties["userID"].ToString();
         }
 
         private void btClicado(object sender, EventArgs e)
         {
-            EnviarNotificacao();
+            if((dataPicker.Date >= DateTime.Now.Date) && (timePicker.Time > DateTime.Now.TimeOfDay))
+               EnviarNotificacao();
+            else
+               DisplayAlert("AVISO", "Data e horário do passado, altere para data e horário futuro!", "OK");
         }
-
-        private static string oneSignalDebugMessage;
 
         private void EnviarNotificacao()
         {
-            var notification = new Dictionary<string, object>();
-            notification["small_icon"] = "wind.png";
-            notification["subtitle"] = new Dictionary<string, string>() { { "en", "Titulo para ios 10+" } };
-            notification["headings"] = new Dictionary<string, string>() { { "en", "Titulo fica aqui" } };
-            notification["contents"] = new Dictionary<string, string>() { { "en", txtNotificacao.Text } };
-            notification["android_background_layout"] = new Dictionary<string, string>() { { "image", "bgImage.jpg" }, { "headings_color", "FFFF0000" }, { "contents_color", "FF0000FF" } };            
+            string dataAgendamento = dataPicker.Date.ToString("yyyy-MM-dd") + " " + timePicker.Time.ToString("hh\\:mm\\:ss") + dataPicker.Date.ToString("zzz");
+            dataAgendamento = Convert.ToDateTime(dataAgendamento).ToUniversalTime().ToString();
 
-            // Just an example userId, use your own or get it the devices by calling OneSignal.GetIdsAvailable
-            notification["include_player_ids"] = new List<string>() { lblIdUser.Text };
+            var notification = new Dictionary<string, object>
+            {
+                ["large_icon"] = "ic_stat_onesignal_default.png",
+                ["android_background_layout"] = new Dictionary<string, string>() { { "image", "onesignal_bgimage_default_image.jpg" }, { "headings_color", "FFFF0000" }, { "contents_color", "FF0000FF" } },
+                ["subtitle"] = new Dictionary<string, string>() { { "en", "Titulo para ios 10+" } },
+                ["headings"] = new Dictionary<string, string>() { { "en", "Titulo fica aqui" } },
+                ["contents"] = new Dictionary<string, string>() { { "en", txtNotificacao.Text } },                
+                ["include_player_ids"] = new List<string>() { lblIdUser.Text }, // Use your own or get it the devices by calling OneSignal.GetIdsAvailable
+                ["send_after"] = dataAgendamento
+            };
 
-            // Example of scheduling a notification in the future.
-            notification["send_after"] = System.DateTime.Now.ToUniversalTime().AddSeconds(10).ToString("U");
+            var resultado = OneSignal.Current.PostNotificationAsync(notification).GetAwaiter().GetResult();
 
-            OneSignal.Current.PostNotification(notification, (responseSuccess) => {
-                oneSignalDebugMessage = "Notification posted successful! Funcionou!.\n" + Json.Serialize(responseSuccess);
-            }, (responseFailure) => {
-                oneSignalDebugMessage = "Notification failed to post: ERRO!\n" + Json.Serialize(responseFailure);
-            });
-
-            lblAviso.Text = oneSignalDebugMessage;
-        }
+            if(resultado.Success)
+                DisplayAlert("AVISO", "Notificação gerada com sucesso!", "OK");
+            else
+                DisplayAlert("ERRO", "Erro ao gerar notificação!", "OK");            
+        }        
     }
 }
